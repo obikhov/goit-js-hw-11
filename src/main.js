927540aafcf6bc7da74ede20070f9b385eba2085
Vladislav <160542917+obikhov@users.noreply.github.com>
2025-01-07 13:20:03 +0200

    dz
    dz

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import { fetchImages } from './js/pixabay-api';
 import { createImageMarkup, clearGallery } from './js/render-functions';
 import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
+
 
 
 
